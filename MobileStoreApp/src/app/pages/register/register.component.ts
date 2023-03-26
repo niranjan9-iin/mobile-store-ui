99e701e3006909a2dce2db96f7c126cfac18609e
Niranjan <kadu.s.niranjan@gmail.com>
2023-03-26 10:30:15 +0530

    changes

--- MobileStoreApp/src/app/pages/register/register.component.ts
+++ MobileStoreApp/src/app/pages/register/register.component.ts
@@ -10,20 +10,24 @@ import { MobileStoreService } from 'src/app/service/mobile-store.service';
 export class RegisterComponent implements OnInit {
 
   errorMessage
-  constructor(public service:MobileStoreService) { }
+  constructor(public service: MobileStoreService) { }
 
   ngOnInit() {
   }
   onSubmit(ngForm: NgForm) {
-    if(ngForm.value.password===ngForm.value.cpassword)
-    {
-      this.service.addUser(ngForm.value).subscribe(res=>{
-       alert('Successfully registered!');
-      },error=>{
-        this.errorMessage = error.error.message
-        alert(this.errorMessage);
-      })
-    }else{
+    const regex = new RegExp('[0-9]{10}');
+    if (ngForm.value.password === ngForm.value.cpassword) {
+      if (regex.test(ngForm.value.mobileNumber)) {
+        this.service.addUser(ngForm.value).subscribe(res => {
+          alert('Successfully registered!');
+        }, error => {
+          this.errorMessage = error.error.message
+          alert(this.errorMessage);
+        })
+      } else {
+        alert('Enter only 10 digits in Mobile Number');
+      }
+    } else {
       alert('Passwords didn\'t match');
     }
   }
